26c6c9d4963cfebcba14f7c884232adcb9cec584
Chris <orj.tsuk@gmail.com>
2022-11-28 23:44:07 -0500

    Added the ability to push new posts to the local array. Some basic styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages });
 });
+
+router.get('/new', function(req, res, next){
+  res.render('form', { title: 'Mini Messageboard'})
+})
+
+router.post('/new', function(req, res, next){
+  req.body.user && req.body.text != null ?
+    messages.push({
+      text: req.body.text,
+      user: req.body.user,
+      added: new Date()
+    }) : null
+    res.redirect('/')
+})
 
 module.exports = router;
